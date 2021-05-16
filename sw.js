650d64f2971ccaa9b4133c549d1d663714ea5bcd
Michael Mao <michaelmao1999@gmail.com>
2021-05-16 16:24:31 -0700

    testing service worker functionality

--- sw.js
+++ sw.js
@@ -7,11 +7,7 @@
 
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  'https://cse110lab6.herokuapp.com/entries',
-  '/',
-  '/style.css',
-  '/scripts/script.js',
-  '/scripts/router.js'
+  'https://cse110lab6.herokuapp.com/entries'
 ];
 
 self.addEventListener('install', function(event) {
